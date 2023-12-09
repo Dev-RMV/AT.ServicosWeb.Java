d4f8f45147594d8d6ad67ffb7e6801f16f0ffc69
Dev-RMV <71718060+Dev-RMV@users.noreply.github.com>
2023-12-09 16:10:19 -0300

    Todos os verbos funcionando, falta consumir API externa e fazer testes )acho que só isso)

--- src/main/java/com/br/DevRmv/At/Java/controller/JogadorController.java
+++ src/main/java/com/br/DevRmv/At/Java/controller/JogadorController.java
@@ -1,11 +1,8 @@
 package com.br.DevRmv.At.Java.controller;
 
-
 import static com.br.DevRmv.At.Java.data.MockData.*;
 
 import com.br.DevRmv.At.Java.model.Jogador;
-import jakarta.annotation.Nullable;
-import org.apache.el.stream.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -13,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -22,39 +20,54 @@ public class JogadorController {
     Logger LOGGER = LoggerFactory.getLogger(JogadorController.class);
 
 
-    @GetMapping("/")
+    @GetMapping
     public List<Jogador> getJogadores() {
-        LOGGER.info("GET->JOGADORES "+jogadores);
+        LOGGER.info("GET->JOGADORES " + jogadores);
         return jogadores;
     }
 
-    @GetMapping("/{cpf}")
-    public ResponseEntity<?> getCpf(@PathVariable  String cpf) {
-        Jogador jogador = jogadores
-                .stream()
-                .filter(j -> Integer.toString(j.getCpf()).equals(cpf))
-                .findFirst()
-                .orElse(null);
-
-        if (jogador == null) {
-            LOGGER.info("GET->CPF "+jogador);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("CPF não encontrado");
+    /*
+     * BUSCA COM 2 PARÂMETROS OPCIONAIS (cpf, nome) COMO PEDIDO NO AT
+     * */
+    @GetMapping("/")
+    public ResponseEntity<?> search(@RequestParam(required = false) String cpf, @RequestParam(required = false) String nome) {
+        List<Jogador> resposta = new ArrayList<>();
+        if (cpf != null) {
+            Jogador jogador = jogadores
+                    .stream()
+                    .filter(j -> Integer.toString(j.getCpf()).equals(cpf))
+                    .findFirst()
+                    .orElse(null);
+            if (jogador != null) {
+                resposta.add(jogador);
+            }
+        }
+        if (nome != null) {
+            jogadores.forEach(jogador -> {
+                if (jogador.getNome().contains(nome) && !(resposta.contains(jogador))) {
+                    resposta.add(jogador);
+                };
+            });
+        };
+        if (resposta.isEmpty()) {
+            LOGGER.info("GET->BUSCA " + resposta);
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Nada foi encontrado");
         } else {
-            LOGGER.info("GET->CPF "+jogador);
-            return ResponseEntity.ok(jogador);
+            LOGGER.info("GET->BUSCA " + resposta);
+            return ResponseEntity.ok(resposta);
         }
     }
 
-    @PostMapping("/")
+    @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<?> insert(@RequestBody Jogador jogador) {
         if (jogador != null) {
             jogador.setHoraCadastro(LocalDateTime.now());
             jogadores.add(jogador);
-            LOGGER.info("POST->INSERIR "+jogador);
+            LOGGER.info("POST->INSERIR " + jogador);
             return ResponseEntity.ok(jogador);
         }
-        LOGGER.info("POST->INSERIR "+jogador);
+        LOGGER.info("POST->INSERIR " + jogador);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não foi possível inserir. Favor rever dados");
     }
 
@@ -67,12 +80,30 @@ public class JogadorController {
                 .orElse(null);
 
         if (jogador == null) {
-            LOGGER.info("DELETE->CPF "+jogador);
+            LOGGER.info("DELETE->CPF " + jogador);
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("CPF não encontrado");
         } else {
-            LOGGER.info("DELETE->CPF "+jogador);
+            LOGGER.info("DELETE->CPF " + jogador);
             jogadores.remove(jogador);
             return ResponseEntity.ok(jogador);
         }
+    }
+
+    @PutMapping("/{cpf}")
+    public ResponseEntity<?> update(@PathVariable int cpf, @RequestBody Jogador jogador) {
+        if (jogador != null) {
+            jogador.setCpf(cpf);
+            jogadores.forEach(item -> {
+                if (item.getCpf() == cpf) {
+                    item.setNome(jogador.getNome());
+                    item.setJogosNaSteam(jogador.getJogosNaSteam());
+                    jogador.setHoraCadastro(item.getHoraCadastro());
+                }
+            });
+            LOGGER.info("POST->UPDATE " + jogador);
+            return ResponseEntity.ok(jogador);
+        }
+        LOGGER.info("POST->UPDATE " + jogador);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Não foi possível fazer update. Favor rever dados");
     }
 }
